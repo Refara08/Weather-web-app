d052ddfdd2a33d68a5152a2f96c3fc99334871ab
Gema Refantero <refara.08@gmail.com>
2022-02-09 16:22:51 +0700

    change http to https at weather.js

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ class Weather {
   //fetching location detail information
   async getGeo(location) {
     const resGeo = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${this.APIkey}`
+      `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${this.APIkey}`
     );
 
     const dataGeo = await resGeo.json();
@@ -18,7 +18,7 @@ class Weather {
 
   async getGeoReverse(lat, lon) {
     const resGeo = await fetch(`
-      http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${this.APIkey}
+      https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${this.APIkey}
     `);
 
     const dataGeo = await resGeo.json();
@@ -31,7 +31,7 @@ class Weather {
   //fetching weather location at specific latitude and longitude
   async getWeather(lat, lon) {
     const resWeather = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${this.APIkey}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${this.APIkey}`
     );
 
     const dataWeather = await resWeather.json();
